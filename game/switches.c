e5b424a12222e5fd8f05adb3a1db805c9cdc2eed
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-05 14:19:44 -0700

    Update switches.c

--- game/switches.c
+++ game/switches.c
@@ -1,33 +1,24 @@
 #include <msp430.h>
 #include "switches.h"
-#include "led.h"
-#include "buzzer.h"
 
-// Global variable for switches
 int switches = 0;
 
-char switch_state_down, switch_state_changed;
-
-// Internal function to update switch interrupt sense
 static char switch_update_interrupt_sense() {
     char p2val = P2IN;
-    P2IES |= (p2val & SWITCHES);   // Sense high-to-low
-    P2IES &= (p2val | ~SWITCHES);  // Sense low-to-high
+    P2IES |= (p2val & SWITCHES);
+    P2IES &= (p2val | ~SWITCHES);
     return p2val;
 }
 
 void switch_init() {
-    P2REN |= SWITCHES;  // Enable resistors for switches
-    P2IE |= SWITCHES;   // Enable interrupts from switches
-    P2OUT |= SWITCHES;  // Pull-ups for switches
-    P2DIR &= ~SWITCHES; // Set switches' bits for input
+    P2REN |= SWITCHES;
+    P2IE |= SWITCHES;
+    P2OUT |= SWITCHES;
+    P2DIR &= ~SWITCHES;
     switch_update_interrupt_sense();
 }
 
 void switch_interrupt_handler() {
     char p2val = switch_update_interrupt_sense();
-
-    __delay_cycles(300000);  // Debounce delay
-
-    switches = ~p2val & SWITCHES; // Update global switches variable
+    switches = ~p2val & SWITCHES;
 }
