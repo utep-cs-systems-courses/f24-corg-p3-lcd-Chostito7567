ae74781150fdf38b9320f36b57ce8d2799b913c6
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-05 14:20:40 -0700

    Update buzzer.h

--- game/buzzer.h
+++ game/buzzer.h
@@ -1,8 +1,11 @@
-#ifndef buzzer_included
-#define buzzer_included
+#ifndef BUZZER_H
+#define BUZZER_H
 
 void buzzer_init();
 void buzzer_set_period(short cycles);
-void buzzer_play_sound();  // Declare the function here
+void play_jingle1();
+void play_jingle2();
+void play_jingle3();
+void play_jingle4();
 
-#endif // buzzer_included
+#endif
