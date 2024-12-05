a583f3cf7bfdac6421703e9f6e4eb27e93632377
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-05 12:33:56 -0700

    Update lcdgame.h

--- game/lcdgame.h
+++ game/lcdgame.h
@@ -1,7 +1,8 @@
 #ifndef LCDGAME_H
 #define LCDGAME_H
 
-void lcd_game_init();            // Initialize the LCD
-void lcd_draw_square();          // Draw a static square
+void lcd_game_init();           // Initialize the LCD
+void lcd_game_play();           // Main game loop
+void play_jingle(int button);   // Play the jingle corresponding to the button
 
 #endif // LCDGAME_H
