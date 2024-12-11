3fd79373c97cb7f38f5204fd4a1a5217956ce045
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 10:16:50 -0700

    Update switches.h

--- game/switches.h
+++ game/switches.h
@@ -1,18 +1,13 @@
-#ifndef SWITCHES_H
-#define SWITCHES_H
+#ifndef switches_included
+#define switches_included
 
-#include <msp430.h>
-
-// Define switch pins
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)
 #define SW1 BIT0
 #define SW2 BIT1
 #define SW3 BIT2
 #define SW4 BIT3
-#define SWITCHES (SW1 | SW2 | SW3 | SW4)
-
-extern int switches;
 
 void switch_init();
 void switch_interrupt_handler();
 
-#endif // SWITCHES_H
+#endif // switches_included
