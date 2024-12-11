8fdfeb61612741513a6fc023825493332b16126a
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 11:04:40 -0700

    Update lcdgame.h

--- game/lcdgame.h
+++ game/lcdgame.h
@@ -1,6 +1,8 @@
 #ifndef LCDGAME_H
 #define LCDGAME_H
 
-void lcd_game_play();
+void lcd_game_display();  // Main function to display the square and numbers
+void lcd_draw_square();   // Function to draw a square
+void lcd_display_numbers(); // Function to display random numbers
 
 #endif // LCDGAME_H
