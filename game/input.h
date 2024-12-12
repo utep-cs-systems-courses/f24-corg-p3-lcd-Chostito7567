7d515b1fa5f5dfdf30e5a7802d95040c548d6257
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 20:21:00 -0700

    Update input.h

--- game/input.h
+++ game/input.h
@@ -1,12 +1,6 @@
-#ifndef INPUT_INCLUDED
-#define INPUT_INCLUDED
+#ifndef INPUT_H
+#define INPUT_H
 
-#define SW1 BIT0  // Button 1
-#define SW2 BIT1  // Button 2
-#define SW3 BIT2  // Button 3
-#define SW4 BIT3  // Button 4
+int input_process(unsigned int* expected_sequence, int sequence_length);
 
-void input_init();  // Initialize the input system
-int input_process(unsigned int* expected_sequence, int sequence_length);  // Process user input
-
-#endif // INPUT_INCLUDED
+#endif // INPUT_H
