1e4d64e698f58eb5462ab1324bf24c8de202b94d
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 21:31:43 -0700

    Update input.c

--- game/input.c
+++ game/input.c
@@ -12,40 +12,40 @@ void input_init() {
 int input_process(unsigned int *expected_sequence, int sequence_length) {
     static int input_index = 0;
 
-    if (switches & SW1) {
-        if (expected_sequence[input_index] == 1) {
-            input_index++;
+    if (switches & SW1) { // If SW1 is pressed
+        if (expected_sequence[user_input_index] == 1) {
+            user_input_index++;
         } else {
-            input_index = 0; // Reset if incorrect
-            return 0; // Incorrect input
+            user_input_index = 0; // Reset on incorrect input
+            return 0; // Incorrect
         }
     } else if (switches & SW2) {
-        if (expected_sequence[input_index] == 2) {
-            input_index++;
+        if (expected_sequence[user_input_index] == 2) {
+            user_input_index++;
         } else {
-            input_index = 0;
+            user_input_index = 0;
             return 0;
         }
     } else if (switches & SW3) {
-        if (expected_sequence[input_index] == 3) {
-            input_index++;
+        if (expected_sequence[user_input_index] == 3) {
+            user_input_index++;
         } else {
-            input_index = 0;
+            user_input_index = 0;
             return 0;
         }
     } else if (switches & SW4) {
-        if (expected_sequence[input_index] == 4) {
-            input_index++;
+        if (expected_sequence[user_input_index] == 4) {
+            user_input_index++;
         } else {
-            input_index = 0;
+            user_input_index = 0;
             return 0;
         }
     }
 
-    if (input_index == sequence_length) {
-        input_index = 0;
-        return 1; // Sequence complete
+    if (user_input_index == sequence_length) {
+        user_input_index = 0; // Reset after successful input
+        return 1; // Correct
     }
 
-    return -1; // In-progress
+    return -1; // Waiting for input
 }
