8cad6d78126e3f5a5a15593ea1ea346912162741
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 22:18:13 -0700

    Update main.c

--- game/main.c
+++ game/main.c
@@ -5,19 +5,21 @@
 #include "switches.h"
 #include "libTimer.h"
 #include "lcdgame.h"
-switch_init();
+#include "input.h"
 
 void main() {
     configureClocks();          // Start Lib Timer
     switch_init();              // Setup Switches
-    buzzer_init();              // Call Speakers
-    led_init();                 // Call LEDs
-
-    lcd_game_init();            // Initialize LCD game
+    buzzer_init();              // Initialize Speakers
+    led_init();                 // Initialize LEDs
+    lcd_game_init();            // Initialize LCD
 
     enableWDTInterrupts();      // Enable Watchdog Timer
 
+    or_sr(0x18); // Power off CPU, enable interrupts
+
     while (1) {
-        lcd_game_display();     // Continuously display game logic
+        input_process();        // Process input
+        lcd_game_display();     // Update the display
     }
 }
