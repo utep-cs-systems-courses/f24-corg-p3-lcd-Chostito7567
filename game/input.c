883c338a3a43947bbbb08ba3444a2db7662b09eb
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 20:24:40 -0700

    Update input.c

--- game/input.c
+++ game/input.c
@@ -1,45 +1,48 @@
-#include <msp430.h>
 #include "input.h"
-#include "switches.h" // Include to access `switches`
+#include "switches.h" // For the `switches` variable
+#include "lcdgame.h"  // For sequence handling
 
-// Process user input
-int input_process(unsigned int* expected_sequence, int sequence_length) {
-    static int current_index = 0;
+void input_init() {
+    // Initialization logic for input (if needed)
+}
 
-    if (switches & SW1) { // If SW1 is pressed
-        if (expected_sequence[current_index] == 1) {
-            current_index++; // Correct input, move to the next
+int input_process(unsigned int *expected_sequence, int sequence_length) {
+    static int input_index = 0;
+
+    if (switches & SW1) {
+        if (expected_sequence[input_index] == 1) {
+            input_index++;
         } else {
-            current_index = 0; // Reset index on incorrect input
-            return 0; // Indicate incorrect input
+            input_index = 0; // Reset if incorrect
+            return 0; // Incorrect input
         }
-    } else if (switches & SW2) { // If SW2 is pressed
-        if (expected_sequence[current_index] == 2) {
-            current_index++;
+    } else if (switches & SW2) {
+        if (expected_sequence[input_index] == 2) {
+            input_index++;
         } else {
-            current_index = 0;
+            input_index = 0;
             return 0;
         }
-    } else if (switches & SW3) { // If SW3 is pressed
-        if (expected_sequence[current_index] == 3) {
-            current_index++;
+    } else if (switches & SW3) {
+        if (expected_sequence[input_index] == 3) {
+            input_index++;
         } else {
-            current_index = 0;
+            input_index = 0;
             return 0;
         }
-    } else if (switches & SW4) { // If SW4 is pressed
-        if (expected_sequence[current_index] == 4) {
-            current_index++;
+    } else if (switches & SW4) {
+        if (expected_sequence[input_index] == 4) {
+            input_index++;
         } else {
-            current_index = 0;
+            input_index = 0;
             return 0;
         }
     }
 
-    if (current_index >= sequence_length) {
-        current_index = 0; // Reset for the next round
-        return 1; // Indicate success
+    if (input_index == sequence_length) {
+        input_index = 0;
+        return 1; // Sequence complete
     }
 
-    return -1; // Indicate in-progress
+    return -1; // In-progress
 }
