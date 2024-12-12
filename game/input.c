e0c65db4de5af58f9c21f7f5f6730555611eec29
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 20:20:44 -0700

    Update input.c

--- game/input.c
+++ game/input.c
@@ -1,42 +1,45 @@
 #include <msp430.h>
 #include "input.h"
-#include "switches.h"
+#include "switches.h" // Include to access `switches`
 
-// Global variables to track the input sequence
-int current_index = 0;
-extern int number_sequence[]; // Array of numbers to match
+// Process user input
+int input_process(unsigned int* expected_sequence, int sequence_length) {
+    static int current_index = 0;
 
-// Process user inputs
-void input_process() {
     if (switches & SW1) { // If SW1 is pressed
-        if (number_sequence[current_index] == 1) {
+        if (expected_sequence[current_index] == 1) {
             current_index++; // Correct input, move to the next
         } else {
             current_index = 0; // Reset index on incorrect input
+            return 0; // Indicate incorrect input
         }
     } else if (switches & SW2) { // If SW2 is pressed
-        if (number_sequence[current_index] == 2) {
+        if (expected_sequence[current_index] == 2) {
             current_index++;
         } else {
             current_index = 0;
+            return 0;
         }
     } else if (switches & SW3) { // If SW3 is pressed
-        if (number_sequence[current_index] == 3) {
+        if (expected_sequence[current_index] == 3) {
             current_index++;
         } else {
             current_index = 0;
+            return 0;
         }
     } else if (switches & SW4) { // If SW4 is pressed
-        if (number_sequence[current_index] == 4) {
+        if (expected_sequence[current_index] == 4) {
             current_index++;
         } else {
             current_index = 0;
+            return 0;
         }
     }
-    
-    // If all inputs are correct
-    if (current_index >= 4) { // Assuming sequence length is 4
+
+    if (current_index >= sequence_length) {
         current_index = 0; // Reset for the next round
-        lcd_game_generate_sequence(); // Generate new sequence
+        return 1; // Indicate success
     }
+
+    return -1; // Indicate in-progress
 }
