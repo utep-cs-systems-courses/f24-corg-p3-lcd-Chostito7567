f0da23750b29dfe8d34686826458f7dea81753e2
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 01:03:46 -0700

    Update stateMachines.c

--- game/stateMachines.c
+++ game/stateMachines.c
@@ -1,53 +1,75 @@
+#include <msp430.h>
 #include "stateMachines.h"
 #include "led.h"
-#include "buzzer.h"
 #include "switches.h"
 
-char current_sequence[] = {1, 2, 3, 4}; // Example sequence
-int sequence_index = 0;
-int user_index = 0;
+static enum {off=0, dim=1, bright=2} ledMode;
+static char pwmCount = 0;
 
-void play_sequence() {
-    // Play the current sequence (using LEDs and buzzer)
-    for (int i = 0; i <= sequence_index; i++) {
-        if (current_sequence[i] == 1) {
-            led_update(LED_RED);
-            buzzer_set_period(1000);
-        } else if (current_sequence[i] == 2) {
-            led_update(LED_GREEN);
-            buzzer_set_period(1500);
-        } else if (current_sequence[i] == 3) {
-            led_update(LED_BLUE);
-            buzzer_set_period(2000);
-        } else if (current_sequence[i] == 4) {
-            led_update(LED_YELLOW);
-            buzzer_set_period(2500);
-        }
-        __delay_cycles(500000); // Wait
-        led_update(0);          // Turn off LED
-        buzzer_set_period(0);   // Turn off buzzer
-        __delay_cycles(250000); // Short pause
-    }
-    sequence_index++; // Increment sequence length
+void sm_slow_clock() {
+  ledMode = (ledMode + 1) % 3;
 }
 
-void check_input() {
-    // Check if the user's input matches the sequence
-    if ((switches & SW1 && current_sequence[user_index] == 1) ||
-        (switches & SW2 && current_sequence[user_index] == 2) ||
-        (switches & SW3 && current_sequence[user_index] == 3) ||
-        (switches & SW4 && current_sequence[user_index] == 4)) {
-        user_index++; // Correct input, move to the next
-        if (user_index > sequence_index) { // Completed sequence
-            play_sequence();
-            user_index = 0;
-        }
-    } else if (switches) {
-        // Incorrect input, reset game
-        buzzer_set_period(500); // Error sound
-        __delay_cycles(1000000);
-        buzzer_set_period(0);
-        sequence_index = 0; // Restart sequence
-        user_index = 0;
-    }
+void sm_fast_clock() {
+  pwmCount = (pwmCount + 1) & 3;
+}
+
+void sm_update_led() {
+  char new_red_on;
+  switch (ledMode) {
+  case off:
+    new_red_on = 0;
+    break;
+  case bright:
+    new_red_on = 1;
+    break;
+  case dim:
+    new_red_on = (pwmCount < 1); // 25% duty cycle
+    break;
+  }
+  if (red_on != new_red_on) {
+    red_on = new_red_on;
+    led_changed = 1;
+  }
+}
+
+char toggle_red() {
+  static char state = 0;
+  switch (state) {
+  case 0:
+    red_on = 1;
+    state = 1;
+    break;
+  case 1:
+    red_on = 0;
+    state = 0;
+    break;
+  }
+  return 1;
+}
+
+char toggle_green() {
+  char changed = 0;
+  if (red_on) {
+    green_on ^= 1;
+    changed = 1;
+  }
+  return changed;
+}
+
+void state_advance() {
+  char changed = 0;
+  static enum {R=0, G=1} color = G;
+  switch (color) {
+  case R: 
+    changed = toggle_red(); 
+    color = G; 
+    break;
+  case G: 
+    changed = toggle_green(); 
+    color = R; 
+    break;
+  }
+  led_changed = changed;
+  led_update();
 }
