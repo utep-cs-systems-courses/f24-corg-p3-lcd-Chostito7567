6ebfb8089a1deaa2b8e1e3ece50891ccd64622ef
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 00:06:32 -0700

    Update led.h

--- game/led.h
+++ game/led.h
@@ -1,16 +1,8 @@
-#ifndef led_included
-#define led_included
+#ifndef LED_H
+#define LED_H
 
-#define LED_RED BIT0       // P1.0
-#define LED_GREEN BIT6     // P1.6
-#define LEDS (BIT0 | BIT6)
+void led_init();
+void led_red_on();
+void led_green_on();
 
-void led_init();           /* Initialize LEDs */
-void led_update();         /* Update LEDs */
-void led_toggle();         /* Toggle LED state */
-void led_advance();        /* Advance LED state */
-
-extern unsigned char led_changed, green_on, red_on, led_state;
-extern char ledFlags;
-
-#endif // led_included
+#endif // LED_H
