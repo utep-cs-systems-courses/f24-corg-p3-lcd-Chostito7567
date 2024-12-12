cfe9c15797b7f7be2fd017936988d23cef7ee368
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 23:53:30 -0700

    Update input.c

--- game/input.c
+++ game/input.c
@@ -1,39 +1,40 @@
-#include "lcdgame.h"
 #include "input.h"
 #include "switches.h"
 
-// Function definition for processing user input
+// Function to process input
 int input_process(unsigned int *expected_sequence, int sequence_length) {
-    static int user_input_index = 0; // Tracks the current position in the sequence
+    static int user_input_index = 0;
 
-    // Check which switch was pressed
-    if (switches & SW1) { // If SW1 is pressed
+    if (switches & SW1) {
         if (expected_sequence[user_input_index++] != 1) {
-            user_input_index = 0; // Reset on incorrect input
-            return 0;             // Return "incorrect"
+            user_input_index = 0;
+            return 0;
         }
-    } else if (switches & SW2) { // If SW2 is pressed
+    } else if (switches & SW2) {
         if (expected_sequence[user_input_index++] != 2) {
             user_input_index = 0;
             return 0;
         }
-    } else if (switches & SW3) { // If SW3 is pressed
+    } else if (switches & SW3) {
         if (expected_sequence[user_input_index++] != 3) {
             user_input_index = 0;
             return 0;
         }
-    } else if (switches & SW4) { // If SW4 is pressed
+    } else if (switches & SW4) {
         if (expected_sequence[user_input_index++] != 4) {
             user_input_index = 0;
             return 0;
         }
     }
 
-    // Check if the entire sequence was successfully input
     if (user_input_index == sequence_length) {
-        user_input_index = 0; // Reset for the next round
-        return 1;             // Return "correct"
+        user_input_index = 0;
+        return 1; // Sequence correct
     }
 
-    return -1; // Return "still waiting for input"
+    return -1; // Waiting for input
+}
+
+void input_init() {
+    // Initialization for input
 }
