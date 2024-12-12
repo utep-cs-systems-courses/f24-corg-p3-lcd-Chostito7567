40a38a8e0600b6e6695b395d3101213d378824d9
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 20:08:21 -0700

    Update main.c

--- game/main.c
+++ game/main.c
@@ -3,25 +3,20 @@
 #include "stateMachines.h"
 #include "buzzer.h"
 #include "switches.h"
-#include "lcdgame.h"
 #include "libTimer.h"
+#include "lcdgame.h"
 
-void main()
-{
-    // Timer
+void main() {
     configureClocks();          // Start Lib Timer
-
-    // Initialize 
     switch_init();              // Setup Switches
     buzzer_init();              // Call Speakers
     led_init();                 // Call LEDs
+
     lcd_game_init();            // Initialize LCD game
 
-    lcd_game_display();         // Display the square with random numbers
-
-    // Watchdog
     enableWDTInterrupts();      // Enable Watchdog Timer
 
-    // Power off CPU
-    or_sr(0x18);
+    while (1) {
+        lcd_game_display();     // Continuously display game logic
+    }
 }
