0cd4fffdb6f9275ebc03bee861a3f3e9cc9b0513
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 00:29:33 -0700

    Update led.h

--- game/led.h
+++ game/led.h
@@ -1,8 +1,15 @@
 #ifndef LED_H
 #define LED_H
 
+#include <msp430.h>
+
+#define LED_RED BIT0
+#define LED_GREEN BIT6
+#define LED_BLUE BIT1
+#define LED_YELLOW BIT2
+#define LEDS (LED_RED | LED_GREEN | LED_BLUE | LED_YELLOW)
+
 void led_init();
-void led_red_on();
-void led_green_on();
+void led_update(char led_state);
 
 #endif // LED_H
