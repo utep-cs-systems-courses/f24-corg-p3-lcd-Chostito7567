c3682a0563944cf28429af5dc8ee973f7096f5d9
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 02:10:56 -0700

    Update lcdgame.c

--- final/lcdgame.c
+++ final/lcdgame.c
@@ -1,7 +1,7 @@
 #include <libTimer.h>
 #include <lcdutils.h>
 #include <lcddraw.h>
-#include <p2switches.h>
+#include <lcdgame.h>
 
 
 /** Initializes everything, clears the screen, draws "hello" and the circle */
