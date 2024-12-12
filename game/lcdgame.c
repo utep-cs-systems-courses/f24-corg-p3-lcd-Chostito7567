c3d47ebe06b70f617c192b3c1f1a4a20f0a9e3e4
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 00:15:49 -0700

    Update lcdgame.c

--- game/lcdgame.c
+++ game/lcdgame.c
@@ -1,22 +1,23 @@
 #include "lcdutils.h"
+#include "lcddraw.h"
 #include "lcdgame.h"
 
 void lcd_game_init() {
-    clearScreen(COLOR_BLACK); // Start with a black screen
+    clearScreen(COLOR_BLACK); // Initialize the screen to black
 }
 
 void lcd_game_display() {
-    clearScreen(COLOR_BLACK);
-    drawChar5x7(30, 50, '1', COLOR_GREEN, COLOR_BLACK); // Display random number
-    drawChar5x7(50, 50, '2', COLOR_RED, COLOR_BLACK);
-    drawChar5x7(70, 50, '3', COLOR_BLUE, COLOR_BLACK);
-    drawChar5x7(90, 50, '4', COLOR_ORANGE, COLOR_BLACK);
+    clearScreen(COLOR_BLACK); // Clear the screen
+    drawChar5x7(10, 20, '1', COLOR_GREEN, COLOR_BLACK); // Display numbers
+    drawChar5x7(30, 20, '2', COLOR_GREEN, COLOR_BLACK);
+    drawChar5x7(50, 20, '3', COLOR_GREEN, COLOR_BLACK);
+    drawChar5x7(70, 20, '4', COLOR_GREEN, COLOR_BLACK);
 }
 
 void lcd_game_display_correct() {
-    clearScreen(COLOR_GREEN); // Full green for correct input
+    clearScreen(COLOR_GREEN); // Display green on success
 }
 
 void lcd_game_display_incorrect() {
-    clearScreen(COLOR_RED); // Full red for incorrect input
+    clearScreen(COLOR_RED); // Display red on failure
 }
