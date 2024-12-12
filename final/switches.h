116f7c3de4b1ae0e79c40f603fa16d7208a0073b
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 01:50:18 -0700

    Update switches.h

--- final/switches.h
+++ final/switches.h
@@ -1,11 +1,15 @@
 #ifndef switches_included
 #define switches_included
 
+#include <msp430.h>
+
 #define SWITCHES (SW1 | SW2 | SW3 | SW4)
 #define SW1 BIT0
 #define SW2 BIT1
 #define SW3 BIT2
 #define SW4 BIT3
+
+extern unsigned int switches; // Declare the global variable
 
 void switch_init();
 void switch_interrupt_handler();
