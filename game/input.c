7a9e320609257d505aa8245fe2e42b3a95c9dbcb
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 22:31:52 -0700

    Update input.c

--- game/input.c
+++ game/input.c
@@ -2,16 +2,40 @@
 #include "lcdgame.h"
 #include "switches.h"
 
-unsigned int current_color = COLOR_ORANGE; // Default background color
+// Process user input to check against the expected sequence
+int input_process(unsigned int *expected_sequence, int sequence_length) {
+    static int user_input_index = 0; // Tracks current position in the sequence
 
-void input_process() {
-    if (switches & SW1) {
-        current_color = COLOR_GREEN; // S1 -> Green
-    } else if (switches & SW2) {
-        current_color = COLOR_RED; // S2 -> Red
-    } else if (switches & SW3) {
-        current_color = COLOR_RED; // S3 -> Red
-    } else if (switches & SW4) {
-        current_color = COLOR_BLACK; // S4 -> Black
+    if (switches & SW1) { // If Switch 1 is pressed
+        if (expected_sequence[user_input_index] != 1) {
+            user_input_index = 0; // Reset on incorrect input
+            return 0; // Incorrect
+        }
+        user_input_index++;
+    } else if (switches & SW2) { // If Switch 2 is pressed
+        if (expected_sequence[user_input_index] != 2) {
+            user_input_index = 0;
+            return 0;
+        }
+        user_input_index++;
+    } else if (switches & SW3) { // If Switch 3 is pressed
+        if (expected_sequence[user_input_index] != 3) {
+            user_input_index = 0;
+            return 0;
+        }
+        user_input_index++;
+    } else if (switches & SW4) { // If Switch 4 is pressed
+        if (expected_sequence[user_input_index] != 4) {
+            user_input_index = 0;
+            return 0;
+        }
+        user_input_index++;
     }
+
+    if (user_input_index >= sequence_length) {
+        user_input_index = 0; // Reset index on correct completion
+        return 1; // Sequence completed correctly
+    }
+
+    return -1; // Input is ongoing
 }
