144c74d40b110a744e924854406b519d44fdc9f4
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 22:28:06 -0700

    Update input.h

--- game/input.h
+++ game/input.h
@@ -2,7 +2,7 @@
 #define INPUT_H
 
 #include "switches.h"
-
+void input_process(); 
 void input_init(); // Initialize input handling
 int input_process(unsigned int *expected_sequence, int sequence_length); // Process user input
 
