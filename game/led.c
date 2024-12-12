df8eeecbf11f6ac4e9823c318014427053546cce
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 01:06:22 -0700

    Update led.c

--- game/led.c
+++ game/led.c
@@ -1,11 +1,25 @@
-#include "led.h"
 #include <msp430.h>
+#include "led.h"
+
+unsigned char led_changed = 0, green_on = 0, red_on = 0, led_state = 0;
 
 void led_init() {
-    P1DIR |= LEDS; // Set LED pins as output
-    led_update(0); // Turn off LEDs
+    P1DIR |= LEDS;  // Set LED pins as output
+    led_changed = 1;
+    led_update();
 }
 
-void led_update(char led_state) {
-    P1OUT = (P1OUT & ~LEDS) | led_state;
+void led_update() {
+    if (led_changed) {
+        char ledFlags = 0;
+        if (led_state == 0) {
+            ledFlags |= LED_RED;
+        } else if (led_state == 1) {
+            ledFlags |= LED_GREEN;
+        } else if (led_state == 2) {
+            ledFlags |= (LED_RED | LED_GREEN);
+        }
+        P1OUT = (P1OUT & ~LEDS) | ledFlags;
+        led_changed = 0;
+    }
 }
