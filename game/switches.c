2bd3c61301a13c687d8466f54f858dbd9b497977
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 00:57:32 -0700

    Update switches.c

--- game/switches.c
+++ game/switches.c
@@ -1,17 +1,18 @@
-#include "switches.h"
 #include <msp430.h>
+#include "switches.h"
 
-char switches = 0;
+unsigned int switches;
 
 void switch_init() {
-    P2REN |= SWITCHES;  // Enable resistors for switches
-    P2IE |= SWITCHES;   // Enable interrupts for switches
-    P2OUT |= SWITCHES;  // Pull-up resistors
-    P2DIR &= ~SWITCHES; // Set switches as input
-    P2IFG &= ~SWITCHES; // Clear pending interrupts
+  P2REN |= SWITCHES;          // Enable resistors for switches
+  P2OUT |= SWITCHES;          // Pull-up resistors
+  P2DIR &= ~SWITCHES;         // Set as input
+  P2IE |= SWITCHES;           // Enable interrupts
+  P2IES |= SWITCHES;          // Trigger on high-to-low transition
+  P2IFG &= ~SWITCHES;         // Clear interrupt flags
 }
 
 void switch_interrupt_handler() {
-    switches = P2IN & SWITCHES; // Update switch state
-    P2IFG &= ~SWITCHES;         // Clear interrupt flags
+  switches = P2IN & SWITCHES; // Read switch values
+  P2IFG &= ~SWITCHES;         // Clear interrupt flags
 }
