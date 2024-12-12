1b5eb552e1d7dbdebb32025067d7f65d747c0b18
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 23:43:27 -0700

    Update input.c

--- game/input.c
+++ game/input.c
@@ -3,40 +3,38 @@
 #include "input.h"
 #include "switches.h"
 
-// Process user input to check against the expected sequence
+// Function definition
 int input_process(unsigned int *expected_sequence, int sequence_length) {
-    static int user_input_index = 0; // Tracks current position in the sequence
+    static int user_input_index = 0; // Tracks the current position in the sequence
 
-    if (switches & SW1) { // If Switch 1 is pressed
-        if (expected_sequence[user_input_index] != 1) {
+    // Check which switch was pressed
+    if (switches & SW1) { // If SW1 is pressed
+        if (expected_sequence[user_input_index++] != 1) {
             user_input_index = 0; // Reset on incorrect input
-            return 0; // Incorrect
+            return 0;             // Return "incorrect"
         }
-        user_input_index++;
-    } else if (switches & SW2) { // If Switch 2 is pressed
-        if (expected_sequence[user_input_index] != 2) {
+    } else if (switches & SW2) { // If SW2 is pressed
+        if (expected_sequence[user_input_index++] != 2) {
             user_input_index = 0;
             return 0;
         }
-        user_input_index++;
-    } else if (switches & SW3) { // If Switch 3 is pressed
-        if (expected_sequence[user_input_index] != 3) {
+    } else if (switches & SW3) { // If SW3 is pressed
+        if (expected_sequence[user_input_index++] != 3) {
             user_input_index = 0;
             return 0;
         }
-        user_input_index++;
-    } else if (switches & SW4) { // If Switch 4 is pressed
-        if (expected_sequence[user_input_index] != 4) {
+    } else if (switches & SW4) { // If SW4 is pressed
+        if (expected_sequence[user_input_index++] != 4) {
             user_input_index = 0;
             return 0;
         }
-        user_input_index++;
     }
 
-    if (user_input_index >= sequence_length) {
-        user_input_index = 0; // Reset index on correct completion
-        return 1; // Sequence completed correctly
+    // Check if the entire sequence was successfully input
+    if (user_input_index == sequence_length) {
+        user_input_index = 0; // Reset for the next round
+        return 1;             // Return "correct"
     }
 
-    return -1; // Input is ongoing
+    return -1; // Return "still waiting for input"
 }
