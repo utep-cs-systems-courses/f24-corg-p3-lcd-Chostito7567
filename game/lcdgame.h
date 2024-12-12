ad86272df08d96527bb0e7cf9f869da2f782240b
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 23:53:18 -0700

    Update lcdgame.h

--- game/lcdgame.h
+++ game/lcdgame.h
@@ -1,14 +1,8 @@
 #ifndef LCDGAME_H
 #define LCDGAME_H
 
-#include "lcdutils.h"
-
-#define GAME_INIT 0
-#define GAME_DISPLAY 1
-#define GAME_WAIT_INPUT 2
-
-void lcd_game_init();    // Initialize the game
-void lcd_game_display(); // Display the random sequence
-void lcd_game_update();  // Update game state and logic
+void lcd_game_init();            // Initialize LCD game
+void lcd_game_display_correct(); // Display "Correct"
+void lcd_game_display_incorrect(); // Display "Incorrect"
 
 #endif // LCDGAME_H
