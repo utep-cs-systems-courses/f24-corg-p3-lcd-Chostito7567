c1022dabcdfdedcaeb8c78fbe267a797f4449478
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 01:05:42 -0700

    Update buzzer.c

--- game/buzzer.c
+++ game/buzzer.c
@@ -1,13 +1,15 @@
-#include "buzzer.h"
 #include <msp430.h>
+#include "buzzer.h"
 
 void buzzer_init() {
-    P2DIR |= BIT6; // Set P2.6 as output
-    P2SEL |= BIT6; // Select P2.6 for timer
-    buzzer_set_period(0);
+    timerAUpmode();                  // Initialize Timer A
+    P2SEL2 &= ~(BIT6 | BIT7);        // Configure P2.6 as output
+    P2SEL &= ~BIT7;
+    P2SEL |= BIT6;
+    P2DIR |= BIT6;
 }
 
 void buzzer_set_period(short cycles) {
     CCR0 = cycles;
-    CCR1 = cycles >> 1; // 50% duty cycle
+    CCR1 = cycles >> 1;              // 50% duty cycle
 }
