375087e84b6bbeb3aa6a9acc8409fa7b27aa1a17
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 00:30:05 -0700

    Update buzzer.c

--- game/buzzer.c
+++ game/buzzer.c
@@ -1,24 +1,13 @@
-#include <msp430.h>
 #include "buzzer.h"
+#include <msp430.h>
 
 void buzzer_init() {
-    P2DIR |= BIT6;
-    P2SEL |= BIT6;
+    P2DIR |= BIT6; // Set P2.6 as output
+    P2SEL |= BIT6; // Select P2.6 for timer
+    buzzer_set_period(0);
 }
 
 void buzzer_set_period(short cycles) {
     CCR0 = cycles;
     CCR1 = cycles >> 1; // 50% duty cycle
-}
-
-void play_correct_buzzer() {
-    buzzer_set_period(1000); // Higher pitch
-    __delay_cycles(1000000);
-    buzzer_set_period(0); // Turn off
-}
-
-void play_incorrect_buzzer() {
-    buzzer_set_period(500); // Lower pitch
-    __delay_cycles(1000000);
-    buzzer_set_period(0); // Turn off
 }
