ed94fffe4cf093eb99d550796e64d01473a1ffc8
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-11 21:35:07 -0700

    Update input.h

--- game/input.h
+++ game/input.h
@@ -1,6 +1,7 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-int input_process(unsigned int* expected_sequence, int sequence_length);
+void input_init();
+int input_process(unsigned int *expected_sequence, int sequence_length);
 
-#endif // INPUT_H
+#endif
