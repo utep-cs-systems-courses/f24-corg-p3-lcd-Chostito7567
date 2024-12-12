d0672fa338c03a2ba7e5a5be4d18d92b29fce92e
Chostito7567 <166172202+Chostito7567@users.noreply.github.com>
2024-12-12 00:29:55 -0700

    Update switches.h

--- game/switches.h
+++ game/switches.h
@@ -3,14 +3,13 @@
 
 #include <msp430.h>
 
-// Define switch macros
 #define SW1 BIT0
 #define SW2 BIT1
 #define SW3 BIT2
 #define SW4 BIT3
-#define SWITCHES (SW1 | SW2 | SW3 | SW4) // Combine all switches
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)
 
-extern unsigned int switches; // Extern declaration for switches variable
+extern char switches;
 
 void switch_init();
 void switch_interrupt_handler();
